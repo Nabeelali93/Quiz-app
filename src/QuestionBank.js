2014e79e583a7b900019733fc4b292f8c9439811
nabeelali <nabeelali720@gmail.com>
2023-08-17 01:46:25 +0500

    first

---
+++ src/QuestionBank.js
@@ -0,0 +1,36 @@
+// QuestionBank.js
+
+const qBank = [
+	{
+		id: 1,
+		question: "What is the capital of Pakistan?",
+		options: ["Karachi", "Lahore", "Islamabad", "Multan"],
+		answer: "Islamabad",
+	},
+	{
+		id: 2,
+		question: "The 17th Asian Games were held in _________?",
+		options: ["2014", "2018", "2019", "2020"],
+		answer: "2014",
+	},
+	{
+		id: 3,
+		question: "Who is the Author of “The Myth of independence”?",
+		options: ["Aysha Jalal", "Zulfikar Ali Bhutto", "I.H. Qureshi", "Nawaz Sharif"],
+		answer: "Zulfikar Ali Bhutto"
+	},
+	{
+		id: 4,
+		question: "How many sports were played in summer Olympic 2016?",
+		options: ["22", "34", "36", "28"],
+		answer: "28"
+	},
+	{
+		id: 5,
+		question: "How many Sports were in Winter Olympic 2018?",
+		options: ["12 Sports", "7 Sports", "15 Sports", "Agra"],
+		answer: "7 Sports"
+	},
+]
+
+export default qBank;
